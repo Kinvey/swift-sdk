748ace3714e007b5edd43371c490c3bbed84e13d
Brian Wilson <brian@kinvey.com>
2012-01-11 21:34:07 +0000

    Fixed #476, had to write logging class from scratch, couldn't find one that did what we needed that we could include.
    git-svn-id: https://subversion.assembla.com/svn/kinvey/ios-library/trunk/KinveyKit@889 868178e2-d38b-4ba8-a603-825f91c87007

---
+++ KinveyKit/Source/KCSLogManager.h
@@ -0,0 +1,72 @@
+//
+//  KCSLogManager.h
+//  KinveyKit
+//
+//  Created by Brian Wilson on 1/11/12.
+//  Copyright (c) 2012 Kinvey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+// Convenience Macros
+#if KINVEY_DEBUG_ENABLED
+
+#define KCSLog(channel,format,...) \
+[[KCSLogManager sharedLogManager] logChannel:(channel) file:__FILE__ lineNumber:__LINE__ \
+withFormat:(format),##__VA_ARGS__]
+
+#define KCSLogNetwork(format,...) \
+[[KCSLogManager sharedLogManager] logChannel:[KCSLogManager kNetworkChannel] file:__FILE__ lineNumber:__LINE__ \
+withFormat:(format),##__VA_ARGS__]
+
+#define KCSLogDebug(format,...) \
+[[KCSLogManager sharedLogManager] logChannel:[KCSLogManager kDebugChannel] file:__FILE__ lineNumber:__LINE__ \
+withFormat:(format),##__VA_ARGS__]
+
+#define KCSLogTrace(format,...) \
+[[KCSLogManager sharedLogManager] logChannel:[KCSLogManager kTraceChannel] file:__FILE__ lineNumber:__LINE__ \
+withFormat:(format),##__VA_ARGS__]
+
+#define KCSLogWarning(format,...) \
+[[KCSLogManager sharedLogManager] logChannel:[KCSLogManager kWarningChannel] file:__FILE__ lineNumber:__LINE__ \
+withFormat:(format),##__VA_ARGS__]
+
+#define KCSLogError(format,...) \
+[[KCSLogManager sharedLogManager] logChannel:[KCSLogManager kErrorChannel] file:__FILE__ lineNumber:__LINE__ \
+withFormat:(format),##__VA_ARGS__]
+
+#else
+
+#define KCSLog(channel,format,...)
+#define KCSLogNetwork(format,...)
+#define KCSLogDebug(format,...)
+#define KCSLogTrace(format,...)
+#define KCSLogWarning(format,...)
+#define KCSLogError(format,...)
+
+#endif
+
+@class KCSLogChannel;
+
+@interface KCSLogManager : NSObject
+
++ (KCSLogManager *)sharedLogManager;
+
++ (KCSLogChannel *)kNetworkChannel;
++ (KCSLogChannel *)kDebugChannel;
++ (KCSLogChannel *)kTraceChannel;
++ (KCSLogChannel *)kWarningChannel;
++ (KCSLogChannel *)kErrorChannel;
+
+
+- (void)logChannel: (KCSLogChannel *)channel file:(char *)sourceFile lineNumber: (int)lineNumber withFormat:(NSString *)format, ...;
+
+- (void)configureLoggingWithNetworkEnabled: (BOOL)networkIsEnabled
+                              debugEnabled: (BOOL)debugIsEnabled
+                              traceEnabled: (BOOL)traceIsEnabled
+                            warningEnabled: (BOOL)warningIsEnabled
+                              errorEnabled: (BOOL)errorIsEnabled;
+
+
+
+@end
---
+++ KinveyKit/Source/KCSLogManager.m
@@ -0,0 +1,196 @@
+//
+//  KCSLogManager.m
+//  KinveyKit
+//
+//  Created by Brian Wilson on 1/11/12.
+//  Copyright (c) 2012 Kinvey. All rights reserved.
+//
+
+#import "KCSLogManager.h"
+
+enum {
+    kKCSDebugChannelID = 1,
+    kKCSTraceChannelID = 2,
+    kKCSWarningChannelID = 3,
+    kKCSErrorChannelID = 4,
+    kKCSNetworkChannelID = 5
+};
+
+@interface KCSLogChannel : NSObject
+
++ (KCSLogChannel *)channelForKey:(NSString *)key;
++ (NSDictionary *)channels;
+
+- (id)initWithDisplayString: (NSString *)displayString channelID:(NSInteger)channelID;
+@property (readonly, nonatomic) NSString *displayString;
+@property (readonly, nonatomic) NSInteger channelID;
+
+@end
+
+@implementation KCSLogChannel
+
+@synthesize displayString = _displayString;
+@synthesize channelID = _channelID;
+
+- (id)initWithDisplayString:(NSString *)displayString channelID:(NSInteger)channelID
+{
+    self = [super init];
+    if (self){
+        _displayString = [displayString retain];
+        _channelID = channelID;
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    [_displayString release];
+}
+
+- (BOOL)isEqual:(id)object
+{
+    return ([self.displayString isEqualToString:[(KCSLogChannel *)object displayString]] &&
+            self.channelID == [(KCSLogChannel *)object channelID]);
+
+}
+
++ (NSDictionary *)channels
+{
+    static NSDictionary *channels = nil;
+    
+    
+    if (channels == nil){
+        channels = [[NSDictionary dictionaryWithObjectsAndKeys:
+                                     [[KCSLogChannel alloc] initWithDisplayString:@"[Network]" channelID:kKCSNetworkChannelID], @"kNetworkChannel",
+                                     [[KCSLogChannel alloc] initWithDisplayString:@"[DEBUG]" channelID:kKCSDebugChannelID], @"kDebugChannel",
+                                     [[KCSLogChannel alloc] initWithDisplayString:@"[Trace]" channelID:kKCSTraceChannelID], @"kTraceChannel",
+                                     [[KCSLogChannel alloc] initWithDisplayString:@"[WARN]" channelID:kKCSWarningChannelID], @"kWarningChannel",
+                                     [[KCSLogChannel alloc] initWithDisplayString:@"[ERROR]" channelID:kKCSErrorChannelID], @"kErrorChannel",
+                                     nil] retain];
+    }
+    
+    return channels;
+}
+
++ (KCSLogChannel *)channelForKey:(NSString *)key
+{
+    return [[KCSLogChannel channels] objectForKey:key];
+}
+
+@end
+
+@interface KCSLogManager ()
+@property (retain, nonatomic) NSDictionary *loggingState;
+@end
+
+@implementation KCSLogManager
+
+@synthesize loggingState = _loggingState;
+
+- (id)init
+{
+    self = [super init];
+    if (self){}
+    return self;
+}
+
++ (KCSLogManager *)sharedLogManager
+{
+    static KCSLogManager *sKCSLogManager;
+    // This can be called on any thread, so we synchronise.  We only do this in 
+    // the sKCSLogManager case because, once sKCSLogManager goes non-nil, it can 
+    // never go nil again.
+    
+    if (sKCSLogManager == nil) {
+        @synchronized (self) {
+            sKCSLogManager = [[KCSLogManager alloc] init];
+            assert(sKCSLogManager != nil);
+        }
+        
+        // Need to seed logging here, all off
+        [sKCSLogManager configureLoggingWithNetworkEnabled:NO
+                                              debugEnabled:NO
+                                              traceEnabled:NO
+                                            warningEnabled:NO
+                                              errorEnabled:NO];
+    }
+    
+    return sKCSLogManager;
+}
+
+
++ (KCSLogChannel *)kNetworkChannel
+{
+    return [KCSLogChannel channelForKey:@"kNetworkChannel"];
+}
+
++ (KCSLogChannel *)kDebugChannel
+{
+    return [KCSLogChannel channelForKey:@"kDebugChannel"];
+}
+
++ (KCSLogChannel *)kTraceChannel
+{
+    return [KCSLogChannel channelForKey:@"kTraceChannel"];
+}
+
++ (KCSLogChannel *)kWarningChannel
+{
+    return [KCSLogChannel channelForKey:@"kWarningChannel"];
+}
+
++ (KCSLogChannel *)kErrorChannel
+{
+    return [KCSLogChannel channelForKey:@"kErrorChannel"];
+}
+
+
+- (void)logChannel: (KCSLogChannel *)channel file:(char *)sourceFile lineNumber: (int)lineNumber withFormat:(NSString *)format, ...
+{
+    BOOL channelIsEnabled = [(NSNumber *)[self.loggingState objectForKey:[NSNumber numberWithInt:channel.channelID]] boolValue];
+    
+    // If the channel is not enabled we don't do anything here
+    if (channelIsEnabled){
+        va_list ap;
+        NSString *print,*file;
+        va_start(ap,format);
+        file=[[NSString alloc] initWithBytes:sourceFile 
+                                      length:strlen(sourceFile) 
+                                    encoding:NSUTF8StringEncoding];
+        print=[[NSString alloc] initWithFormat:format arguments:ap];
+        va_end(ap);
+        //NSLog handles synchronization issues
+        NSLog(@"%s:%d %@ %@",[[file lastPathComponent] UTF8String],
+              lineNumber, channel.displayString,print);
+        [print release];
+        [file release];
+    }
+}
+
+- (void)configureLoggingWithNetworkEnabled:(BOOL)networkIsEnabled 
+                              debugEnabled:(BOOL)debugIsEnabled 
+                              traceEnabled:(BOOL)traceIsEnabled
+                            warningEnabled:(BOOL)warningIsEnabled
+                              errorEnabled:(BOOL)errorIsEnabled
+{
+    NSNumber *netChan = [NSNumber numberWithInt:[[KCSLogManager kNetworkChannel] channelID]];
+    NSNumber *dbgChan = [NSNumber numberWithInt:[[KCSLogManager kDebugChannel] channelID]];
+    NSNumber *trcChan = [NSNumber numberWithInt:[[KCSLogManager kTraceChannel] channelID]];
+    NSNumber *wrnChan = [NSNumber numberWithInt:[[KCSLogManager kWarningChannel] channelID]];
+    NSNumber *errChan = [NSNumber numberWithInt:[[KCSLogManager kErrorChannel] channelID]];
+    
+    NSDictionary *configuredStates = [NSDictionary dictionaryWithObjectsAndKeys:
+                                      [NSNumber numberWithBool:networkIsEnabled], netChan,
+                                      [NSNumber numberWithBool:debugIsEnabled], dbgChan,
+                                      [NSNumber numberWithBool:traceIsEnabled], trcChan,
+                                      [NSNumber numberWithBool:warningIsEnabled], wrnChan,
+                                      [NSNumber numberWithBool:errorIsEnabled], errChan,
+                                      nil];
+    
+    self.loggingState = configuredStates;
+}
+
+
+
+
+@end
