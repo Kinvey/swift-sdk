3b27d8c0ad8a3ae15cd63dfb72f03e82c4933469
Brian Wilson <brian@kinvey.com>
2012-03-17 16:03:01 +0000

    re #610 Fixed bug in included test library (referenced incorrect test framework), first pass fix of many if not all known memory leaks
    git-svn-id: https://subversion.assembla.com/svn/kinvey/ios-library/trunk@1424 868178e2-d38b-4ba8-a603-825f91c87007
    
    Former-commit-id: d56466c0dc5ec62671821b95c26bea2901a726f4

--- KinveyKit/KinveyKitTests/KinveyKitUserTests.h
+++ KinveyKit/KinveyKitTests/KinveyKitUserTests.h
@@ -11,7 +11,7 @@
 
 #import <SenTestingKit/SenTestingKit.h>
 #define HC_SHORTHAND
-#import <OCHamcrest/OCHamcrest.h>
+#import <OCHamcrestIOS/OCHamcrestIOS.h>
 
 #import "KinveyUser.h"
 #import "KinveyPersistable.h"
