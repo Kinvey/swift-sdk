a2cc6a2817b13b84c2c94b164d44ac1a6bbf5724
Brian Wilson <brian@kinvey.com>
2012-03-19 20:08:36 +0000

    re #610 -- More fixes and improvements
    git-svn-id: https://subversion.assembla.com/svn/kinvey/ios-library/trunk@1437 868178e2-d38b-4ba8-a603-825f91c87007
    
    Former-commit-id: 597cb28493c11191d7b6217b1906b981a887896a

--- KinveyKit/KinveyKit/Source/KCSEntityDict.m
+++ KinveyKit/KinveyKit/Source/KCSEntityDict.m
@@ -8,15 +8,70 @@
 
 #import "KCSEntityDict.h"
 
+// Private interface
+@interface KCSEntityDict ()
+@property (nonatomic, retain) NSMutableDictionary *entityProperties;
+@end
 
 
 
 @implementation KCSEntityDict
 
+@synthesize entityProperties = _entityProperties;
+@synthesize objectId = _objectId;
 
-- (NSDictionary*)propertyToElementMapping
+- (id)init
 {
-    return nil;
+    self = [super init];
+    if (self) {
+        _entityProperties = [[NSMutableDictionary alloc] init];
+        _objectId = [[NSString alloc] init];
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    [_entityProperties release];
+    [_objectId release];
+    [super dealloc];
+}
+
+- (id)getValueForProperty: (NSString *)property
+{
+    return [self.entityProperties objectForKey:property];
+}
+
+- (void)setValue: (id)value forProperty:(NSString *)property
+{
+    [self.entityProperties setObject:value forKey:property];
+}
+
+
+
++ (NSDictionary *)kinveyObjectBuilderOptions
+{
+    static NSDictionary *options = nil;
+    
+    if (options == nil){
+        options = [[NSDictionary dictionaryWithObjectsAndKeys:
+                    [NSNumber numberWithBool:YES], KCS_USE_DICTIONARY_KEY,
+                    @"entityProperties", KCS_DICTIONARY_NAME_KEY, nil] retain];
+    }
+    
+    return options;
+}
+
+- (NSDictionary *)hostToKinveyPropertyMapping
+{
+    static NSDictionary *mappedDict = nil;
+    
+    if (mappedDict == nil){
+        mappedDict = [[NSDictionary dictionaryWithObjectsAndKeys:
+                       @"_id", @"objectId", nil] retain];
+    }
+    
+    return mappedDict;
 }
 
 
