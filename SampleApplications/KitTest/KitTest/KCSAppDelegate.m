38dbbc716d0bec09793f1aff67b4f70faca8964c
Brian Wilson <brian@kinvey.com>
2012-03-20 01:45:26 +0000

    re #610 -- Updated sample code to point to console-staging.  Works.
    git-svn-id: https://subversion.assembla.com/svn/kinvey/ios-library/trunk@1441 868178e2-d38b-4ba8-a603-825f91c87007

--- SampleApplications/KitTest/KitTest/KCSAppDelegate.m
+++ SampleApplications/KitTest/KitTest/KCSAppDelegate.m
@@ -67,7 +67,7 @@
     
     [[KCSClient sharedClient] initializeKinveyServiceForAppKey:[options valueForKey:KCS_APP_KEY_KEY] withAppSecret:[options valueForKey:KCS_APP_SECRET_KEY] usingOptions:options];
     [[KCSPush sharedPush] onLoadHelper:options];
-    [[KCSClient sharedClient] setServiceHostname:@"latestbeta"];
+    [[KCSClient sharedClient] setServiceHostname:@"console-staging"];
 
     
 //    [[[KCSClient sharedClient] currentUser] logout];
