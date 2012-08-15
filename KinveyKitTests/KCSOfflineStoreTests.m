e1fbc53a3012ba2bfa6014574f2cd66996d40b63
Michael Katz <michael@kinvey.com>
2012-08-15 20:28:43 +0000

    get shouldsave delegate working RE #527
    git-svn-id: https://subversion.assembla.com/svn/kinvey/ios-library/trunk/KinveyKit@3159 868178e2-d38b-4ba8-a603-825f91c87007
    
    Former-commit-id: 884010407dd96cc84bb4593132ebcce0accbe673

--- KinveyKitTests/KCSOfflineStoreTests.m
+++ KinveyKitTests/KCSOfflineStoreTests.m
@@ -12,12 +12,27 @@
 #import "ASTTestClass.h"
 #import "KCSHiddenMethods.h"
 
+@interface KCSAppdataStore ()
+- (void) setReachable:(BOOL)r;
+@end
+
+@interface KCSOfflineStoreTests ()
+{
+    BOOL _shouldSaveCalled;
+    BOOL _shouldSaveReturn;
+}
+
+@end
+
 @implementation KCSOfflineStoreTests
 
 - (void)setUp
 {
     BOOL up = [TestUtils setUpKinveyUnittestBackend];
     STAssertTrue(up, @"should be setup");
+    
+    _shouldSaveCalled = NO;
+    _shouldSaveReturn = YES;
 }
 
 - (void) testErrorOnOffline
@@ -29,19 +44,62 @@
     obj.objCount = 79000;
     
     KCSCollection* c = [TestUtils randomCollection:[ASTTestClass class]];
-    KCSAppdataStore* store = [KCSAppdataStore storeWithCollection:c options:@{}];
+    KCSAppdataStore* store = [KCSAppdataStore storeWithCollection:c options:@{KCSStoreKeyUniqueOfflineSaveIdentifier : @"x"}];
     
     [store setReachable:NO];
     
     self.done = NO;
     [store saveObject:obj withCompletionBlock:^(NSArray *objectsOrNil, NSError *errorOrNil) {
         STAssertError(errorOrNil, KCSKinveyUnreachableError);
+        NSArray* objs = [[errorOrNil userInfo] objectForKey:KCS_ERROR_UNSAVED_OBJECT_IDS_KEY];
+        STAssertEquals((NSUInteger)1, (NSUInteger) objs.count, @"should have one unsaved obj, from above");
         self.done = YES;
     } withProgressBlock:^(NSArray *objects, double percentComplete) {
         NSLog(@"%f", percentComplete);
     }];
     
     [self poll];
+}
+
+- (void) testWillSaveWhenGoBackOnline
+{
+    ASTTestClass* obj = [[ASTTestClass alloc] init];
+    obj.date = [NSDate date];
+    obj.objCount = 79000;
+    
+    KCSCollection* c = [TestUtils randomCollection:[ASTTestClass class]];
+    KCSOfflineStoreTests* o = self;
+    KCSAppdataStore* store = [KCSAppdataStore storeWithCollection:c options:@{KCSStoreKeyUniqueOfflineSaveIdentifier : @"x", KCSStoreKeyOfflineSaveDelegate : o}];
+    
+    [store setReachable:NO];
+    
+    self.done = NO;
+    [store saveObject:obj withCompletionBlock:^(NSArray *objectsOrNil, NSError *errorOrNil) {
+        STAssertError(errorOrNil, KCSKinveyUnreachableError);
+        NSArray* objs = [[errorOrNil userInfo] objectForKey:KCS_ERROR_UNSAVED_OBJECT_IDS_KEY];
+        STAssertEquals((int)1, objs.count, @"should have one unsaved obj, from above");
+        self.done = YES;
+    } withProgressBlock:^(NSArray *objects, double percentComplete) {
+        NSLog(@"%f", percentComplete);
+    }];
+    
+    [self poll];
+    
+    self.done = NO;
+    
+    [store setReachable:YES];
+    
+    [self poll];
+}
+
+#pragma mark - Offline Save Delegate
+- (BOOL)shouldSave:(id<KCSPersistable>)entity lastSaveTime:(NSDate *)timeSaved
+{
+    _shouldSaveCalled = YES;
+    ASTTestClass* obj = entity;
+    STAssertEquals((int)79000, obj.objCount, @"should have the right obj to save");
+    self.done = YES;
+    return _shouldSaveReturn;
 }
 
 @end
