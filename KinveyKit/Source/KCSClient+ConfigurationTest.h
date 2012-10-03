0b1b1f2a920cd9791125366259f3c9d0dd7251d6
Michael Katz <michael@kinvey.com>
2012-10-03 16:48:44 +0000

    fixes #1548
    git-svn-id: https://subversion.assembla.com/svn/kinvey/ios-library/trunk/KinveyKit@3846 868178e2-d38b-4ba8-a603-825f91c87007
    
    Former-commit-id: 4d39dadd2e2eea275aca43646e801b72230cf49a

---
+++ KinveyKit/Source/KCSClient+ConfigurationTest.h
@@ -0,0 +1,15 @@
+//
+//  KCSClient+ConfigurationTest.h
+//  KinveyKit
+//
+//  Created by Michael Katz on 10/3/12.
+//  Copyright (c) 2012 Kinvey. All rights reserved.
+//
+
+#import <KinveyKit/KinveyKit.h>
+
+@interface KCSClient (ConfigurationTest)
+
+- (void) testCanUseCategories;
+
+@end
