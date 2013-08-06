a4b323ca21c8390f3771801301ac6d19036bd9c2
Michael Katz <michael@kinvey.com>
2013-08-06 18:46:11 +0000

    update KitTest for files re #2543
    git-svn-id: https://subversion.assembla.com/svn/kinvey/ios-library/trunk@7446 868178e2-d38b-4ba8-a603-825f91c87007
    
    Former-commit-id: 5b508f39d2d527af09e6fa934d22afbc2425d13a

--- SampleApplications/KitTest/KitTest/KinveyRefViewController.m
+++ SampleApplications/KitTest/KitTest/KinveyRefViewController.m
@@ -3,17 +3,32 @@
 //  KitTest
 //
 //  Created by Michael Katz on 9/7/12.
-//  Copyright (c) 2012 Kinvey. All rights reserved.
+//  Copyright (c) 2012-2013 Kinvey. All rights reserved.
+//
+// This software is licensed to you under the Kinvey terms of service located at
+// http://www.kinvey.com/terms-of-use. By downloading, accessing and/or using this
+// software, you hereby accept such terms of service  (and any agreement referenced
+// therein) and agree that you have read, understand and agree to be bound by such
+// terms of service and are of legal age to agree to such terms with Kinvey.
+//
+// This software contains valuable confidential and proprietary information of
+// KINVEY, INC and is subject to applicable licensing agreements.
+// Unauthorized reproduction, transmission or distribution of this file and its
+// contents is a violation of applicable laws.
 //
 
 #import "KinveyRefViewController.h"
-@class LeftObj;
+@interface LeftObj : NSObject <KCSPersistable>
+@property (nonatomic, retain) NSString* objId;
+@property (nonatomic, retain) NSArray* rightObjs;
+@end
+
 @interface RightObj : NSObject <KCSPersistable>
 @property (nonatomic, retain) NSString* objId;
 @property (nonatomic, retain) NSArray* leftObjs;
 @end
 @implementation RightObj
-@synthesize objId,leftObjs;
+
 - (NSDictionary *)hostToKinveyPropertyMapping
 {
     return @{@"objId" : KCSEntityKeyId, @"leftObjs" : @"leftObj"};
@@ -32,12 +47,10 @@
     return [NSString stringWithFormat:@"<%@>",self.objId];
 }
 @end
-@interface LeftObj : NSObject <KCSPersistable>
-@property (nonatomic, retain) NSString* objId;
-@property (nonatomic, retain) NSArray* rightObjs;
-@end
+
+
 @implementation LeftObj
-@synthesize rightObjs, objId;
+
 - (NSDictionary *)hostToKinveyPropertyMapping
 {
     return @{@"objId" : KCSEntityKeyId, @"rightObjs" : @"rightObj"};
