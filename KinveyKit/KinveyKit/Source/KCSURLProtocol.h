e7fdb2eafe12ef33add8ca17c1915d180fc1ac90
Victor Barros <victor@kinvey.com>
2015-03-26 13:40:54 -0400

    adding documentation
    Former-commit-id: 30c5018d34c156c3aa9100869f868fdaf219c90a

--- KinveyKit/KinveyKit/Source/KCSURLProtocol.h
+++ KinveyKit/KinveyKit/Source/KCSURLProtocol.h
@@ -8,12 +8,35 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ NSURLProtocol wrapper for Kinvey requests.
+ @since 1.29.0
+ */
 @interface KCSURLProtocol : NSURLProtocol
 
+/**
+ Register the protocol class in the NSURLProtocol and add the protocol class for Kinvey requests.
+ 
+ @param protocolClass the protocol class to be registered.
+ @return YES if the protocol was registered successfully.
+ @since 1.29.0
+ */
 +(BOOL)registerClass:(Class)protocolClass;
 
+/**
+ Unregister the protocol class in the NSURLProtocol and remove the protocol class for Kinvey requests.
+ 
+ @param protocolClass the protocol class to be registered.
+ @since 1.29.0
+ */
 +(void)unregisterClass:(Class)protocolClass;
 
+/**
+ Returns all registered protocol classes for Kinvey requests.
+ 
+ @return all registered protocol classes for Kinvey requests
+ @since 1.29.0
+ */
 +(NSArray*)protocolClasses;
 
 @end
