6379e6a11e7fad8503def6bf9b1ea3a2a926dff1
Victor Barros <victor@kinvey.com>
2015-08-18 15:42:38 -0700

    enabling unit tests one by one
    Former-commit-id: e6dee51b7aec4e96c84cf77ef0b0cbaf3dcf0a55

--- KinveyKit/KinveyKitTests/LogTests.m
+++ KinveyKit/KinveyKitTests/LogTests.m
@@ -29,18 +29,6 @@
 
 @implementation LogTests
 
-- (void)setUp
-{
-    [super setUp];
-    // Put setup code here; it will be run once, before the first test case.
-}
-
-- (void)tearDown
-{
-    // Put teardown code here; it will be run once, after the last test case.
-    [super tearDown];
-}
-
 - (void)testDefaultLevel
 {
     KCSClientConfiguration* config = [KCSClientConfiguration configurationWithAppKey:@"A" secret:@"S"];
