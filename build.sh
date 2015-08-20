66cffb43e8e771f5bffc4d5561885fedb58b0c6a
Victor Barros <victor@kinvey.com>
2015-08-20 15:03:25 -0700

    killing simulators before start the test

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@ PROJECT="KinveyKit"
 SCHEME="Kinvey"
 REPO_ROOT=$1
 ps aux | grep _sim | grep -v grep | awk '{print $2}' | xargs kill -9 2>/dev/null
+osascript kill-simulators
 xcodebuild -project $REPO_ROOT/$PROJECT/$PROJECT.xcodeproj -scheme $SCHEME clean build test | xcpretty -tc -r junit -r html
