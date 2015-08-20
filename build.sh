3e3df8e2fac12e1c83d32c673dfbe97fd89e2e29
Victor Barros <victor@kinvey.com>
2015-08-20 14:58:28 -0700

    killing simulators before start the test
    Former-commit-id: 070f0286ccdd6097b8a0f44dbf2f72ec2749eed9

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 PROJECT="KinveyKit"
 SCHEME="Kinvey"
 REPO_ROOT=$1
+ps aux | grep _sim | grep -v grep | awk '{print $2}' | xargs kill -9 2>/dev/null
 xcodebuild -project $REPO_ROOT/$PROJECT/$PROJECT.xcodeproj -scheme $SCHEME clean build test | xcpretty -tc -r junit -r html
