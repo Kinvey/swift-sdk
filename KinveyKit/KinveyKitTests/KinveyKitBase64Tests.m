3e32b3557f76185e1d75874a14761ec4de148bf2
Victor Barros <victor@kinvey.com>
2015-10-13 15:00:45 -0700

    removing base64 code and start using the Apple's implementation provided since iOS 7+

--- KinveyKit/KinveyKitTests/KinveyKitBase64Tests.m
+++ KinveyKit/KinveyKitTests/KinveyKitBase64Tests.m
@@ -15,7 +15,7 @@
 {
     NSString *expectedString = @"QWxhZGRpbjpvcGVuIHNlc2FtZQ==";
     NSString *string = @"Aladdin:open sesame";
-    NSString *b64 = KCSbase64EncodedStringFromData([string dataUsingEncoding:NSUTF8StringEncoding]);
+    NSString *b64 = [[string dataUsingEncoding:NSUTF8StringEncoding] base64EncodedStringWithOptions:0];
     
     XCTAssertEqualObjects(b64, expectedString, @"");
 }
@@ -24,7 +24,7 @@
 {
     NSString *string = @"QWxhZGRpbjpvcGVuIHNlc2FtZQ==";
     NSString *expectedString = @"Aladdin:open sesame";
-    NSData *b64 = KCSdataFromBase64String(string);
+    NSData *b64 = [[NSData alloc] initWithBase64EncodedString:string options:0];
     NSString *actual = [NSString stringWithUTF8String:[b64 bytes]];
     
     XCTAssertEqualObjects(actual, expectedString, @"");
@@ -34,13 +34,20 @@
 {
     NSString *expectedString = @"N0M5MUU2RTMtNDlFMy01RUNELUI2NjAtNUU5NDA0REVEMTUwOmIzOTI3MjM4LTViNTctNDQ5ZS1hMjdmLWZiNjM3ZDhhNWU4Yg==";
     NSString *string = @"7C91E6E3-49E3-5ECD-B660-5E9404DED150:b3927238-5b57-449e-a27f-fb637d8a5e8b";
-    NSString *b64 = KCSbase64EncodedStringFromData([string dataUsingEncoding:NSUTF8StringEncoding]);
+    NSString *b64 = [[string dataUsingEncoding:NSUTF8StringEncoding] base64EncodedStringWithOptions:0];
     XCTAssertEqualObjects(b64, expectedString, @"");
     
     // Round trip
-    NSData *bd64 = KCSdataFromBase64String(expectedString);
+    NSData *bd64 = [[NSData alloc] initWithBase64EncodedString:expectedString options:0];
     NSString *actual = [[NSString alloc] initWithData:bd64 encoding:NSUTF8StringEncoding];
     XCTAssertEqualObjects(actual, string, @"");
+}
+
+-(void)testBasicAuthString
+{
+    NSString *expectedString = @"Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ==";
+    NSString *b64 = KCSbasicAuthString(@"Aladdin", @"open sesame");
+    XCTAssertEqualObjects(b64, expectedString);
 }
 
 @end
