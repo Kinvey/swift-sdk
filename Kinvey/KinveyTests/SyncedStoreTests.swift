ab188b6105df487272ca214a333360cd5831d648
Victor Barros <victor@kinvey.com>
2015-12-18 00:05:13 -0800

    persistence cache using Realm for Cached and Synced Stores

---
+++ Kinvey/KinveyTests/SyncedStoreTests.swift
@@ -0,0 +1,20 @@
+//
+//  SyncedStoreTests.swift
+//  Kinvey
+//
+//  Created by Victor Barros on 2015-12-17.
+//  Copyright © 2015 Kinvey. All rights reserved.
+//
+
+import XCTest
+@testable import Kinvey
+
+class SyncedStoreTests: NetworkStoreTests {
+    
+    override func setUp() {
+        super.setUp()
+        
+        store = SyncedStore<Person>(client: client)
+    }
+    
+}
