6f005dea11da8b23f398a41cb0b27c051dbcf0ed
Victor Barros <victor@kinvey.com>
2016-02-11 23:03:15 -0800

    separate project for MongoDBPredicateAdaptor

---
+++ Kinvey/Dependencies/modules/NSPredicate-MongoDB-Adaptor/MongoDBPredicateAdaptorTests/MongoDBPredicateAdaptorTests.m
@@ -0,0 +1,38 @@
+//
+//  MongoDBPredicateAdaptorTests.m
+//  MongoDBPredicateAdaptorTests
+//
+//  Created by Victor Barros on 2016-02-11.
+//  Copyright © 2016 tjboneman. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import <NSPredicateMongoDBAdaptorKit/NSPredicateMongoDBAdaptorKit.h>
+
+@interface MongoDBPredicateAdaptorTests : XCTestCase
+
+@end
+
+@implementation MongoDBPredicateAdaptorTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    NSPredicate* predicate = [NSPredicate predicateWithFormat:@"name == %@", @"Victor"];
+    NSError* error = nil;
+    NSDictionary* result = [MongoDBPredicateAdaptor queryDictFromPredicate:predicate
+                                                                   orError:&error];
+    XCTAssertNil(error);
+    XCTAssertNotNil(result);
+    XCTAssertEqualObjects(result, @{@"name" : @"Victor"});
+}
+
+@end
