08fdfa8fb68617ebcebb352ec8e3055629863080
Victor Barros <victor@kinvey.com>
2016-03-30 23:00:22 -0700

    adding support for custom endpoints and $size queries

---
+++ Kinvey/Kinvey/CustomEndpoint.swift
@@ -0,0 +1,48 @@
+//
+//  Command.swift
+//  Kinvey
+//
+//  Created by Thomas Conner on 3/15/16.
+//  Copyright © 2016 Kinvey. All rights reserved.
+//
+
+import Foundation
+
+@objc(KNVCustomEndpoint)
+public class CustomEndpoint: NSObject {
+    
+    public typealias CompletionHandler = (JsonDictionary?, ErrorType?) -> Void
+    
+    public static func execute(name: String, params: JsonDictionary? = nil, client: Client = sharedClient, completionHandler: CompletionHandler? = nil) -> Request {
+        let request = client.networkRequestFactory.buildCustomEndpoint(name)
+        if let params = params {
+            request.request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.request.HTTPBody = try! NSJSONSerialization.dataWithJSONObject(params.toJson(), options: [])
+        }
+        request.execute() { data, response, error in
+            if let completionHandler = dispatchAsyncMainQueue(completionHandler) {
+                if let response = response where response.isResponseOK, let json = client.responseParser.parse(data) {
+                    completionHandler(json, nil)
+                } else if let error = error {
+                    completionHandler(nil, error)
+                } else {
+                    completionHandler(nil, Error.InvalidResponse)
+                }
+            }
+        }
+        return request
+    }
+    
+    //MARK: Dispatch Async Main Queue
+    
+    private static func dispatchAsyncMainQueue<R>(completionHandler: ((R?, ErrorType?) -> Void)? = nil) -> ((JsonDictionary?, ErrorType?) -> Void)? {
+        if let completionHandler = completionHandler {
+            return { (obj, error) -> Void in
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    completionHandler(obj as? R, error)
+                })
+            }
+        }
+        return nil
+    }
+}
