33fd92207e919297315407416a9230bdad05a6ca
Victor Barros <victor@kinvey.com>
2016-05-03 10:20:43 -0400

    adding busber sample project to the workspace
    Former-commit-id: e6915922762b75368580fb8cfd145d13b0b646e0

---
+++ SampleApplications/BuSber/BuSber/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BuSber
+//
+//  Created by Victor Barros on 2016-05-03.
+//  Copyright © 2016 Kinvey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
