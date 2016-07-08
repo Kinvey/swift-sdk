e6cef8011e8726c069d9cf86a78a0408c5d930d7
Victor Barros <victor@kinvey.com>
2016-07-08 11:37:50 -0700

    event class for unit tests

--- Kinvey/KinveyTests/NetworkStoreTests.swift
+++ Kinvey/KinveyTests/NetworkStoreTests.swift
@@ -13,12 +13,45 @@ class NetworkStoreTests: StoreTestCase {
     
     override func setUp() {
         super.setUp()
+        signUp()
         
         store = DataStore<Person>.getInstance()
     }
     
     override func assertThread() {
         XCTAssertTrue(NSThread.isMainThread())
+    }
+    
+    func testSaveEvent() {
+        let store = DataStore<Event>.getInstance(.Network)
+        
+        let event = Event()
+        event.name = "Friday Party!"
+        event.date = NSDate(timeIntervalSince1970: 1468001397) // Fri, 08 Jul 2016 18:09:57 GMT
+        event.location = "The closest pub!"
+        
+        event.acl?.globalRead.value = true
+        event.acl?.globalWrite.value = true
+        
+        let myFriend: User // set to a valid User
+        event.acl?.readers?.append(myFriend.userId)
+        
+        //add 'myFriend' to the writers list as well
+        event.acl?.writers?.append(myFriend.userId)
+        //... and then save the event
+        
+        weak var expectationCreate = expectationWithDescription("Create")
+        
+        store.save(event) { event, error in
+            XCTAssertNotNil(event)
+            XCTAssertNil(error)
+            
+            expectationCreate?.fulfill()
+        }
+        
+        waitForExpectationsWithTimeout(defaultTimeout) { error in
+            expectationCreate = nil
+        }
     }
     
 }
