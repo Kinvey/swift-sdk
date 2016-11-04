be9539f594a7f0b7e4dddadc149873fe19942907
Victor Barros <victor@kinvey.com>
2016-11-03 18:25:12 -0700

    MLIBZ-1324: bugfix for migration between schema versions

--- Kinvey/Kinvey/SyncManager.swift
+++ Kinvey/Kinvey/SyncManager.swift
@@ -13,14 +13,16 @@ internal class SyncManager: NSObject {
     
     fileprivate let persistenceId: String
     fileprivate let encryptionKey: Data?
+    fileprivate let schemaVersion: UInt64
     
-    init(persistenceId: String, encryptionKey: Data? = nil) {
+    init(persistenceId: String, encryptionKey: Data? = nil, schemaVersion: UInt64) {
         self.persistenceId = persistenceId
         self.encryptionKey = encryptionKey
+        self.schemaVersion = schemaVersion
     }
     
-    func sync<T: Persistable>(filePath: String? = nil, type: T.Type) -> Sync<T> where T: NSObject {
-        return RealmSync<T>(persistenceId: persistenceId, filePath: filePath, encryptionKey: encryptionKey)
+    func sync<T: Persistable>(fileURL: URL? = nil, type: T.Type) -> Sync<T> where T: NSObject {
+        return RealmSync<T>(persistenceId: persistenceId, fileURL: fileURL, encryptionKey: encryptionKey, schemaVersion: schemaVersion)
     }
     
 }
