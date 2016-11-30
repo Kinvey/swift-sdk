de7199c12965c922b15f6c78b5c07374eb8b7ac7
Victor Barros <victor@kinvey.com>
2016-11-29 21:13:14 -0800

    Adding unit tests
    Former-commit-id: 35ecca0e4194b44a4089c808df4be0521187eb34

---
+++ Kinvey/KinveyTests/CacheStoreTests.swift
@@ -0,0 +1,67 @@
+//
+//  CachedStoreTests.swift
+//  Kinvey
+//
+//  Created by Victor Barros on 2015-12-15.
+//  Copyright © 2015 Kinvey. All rights reserved.
+//
+
+import XCTest
+@testable import Kinvey
+
+class CacheStoreTests: StoreTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        
+        signUp()
+        
+        store = DataStore<Person>.collection(.cache)
+    }
+    
+    func testSaveAddress() {
+        let person = Person()
+        person.name = "Victor Barros"
+        
+        weak var expectationSaveLocal = expectation(description: "Save Local")
+        weak var expectationSaveNetwork = expectation(description: "Save Network")
+        
+        var runCount = 0
+        
+        store.save(person) { person, error in
+            XCTAssertNotNil(person)
+            XCTAssertNil(error)
+            
+            switch runCount {
+            case 0:
+                if let person = person {
+                    XCTAssertNotNil(person.personId)
+                    if let personId = person.personId {
+                        XCTAssertTrue(personId.hasPrefix(ObjectIdTmpPrefix))
+                    }
+                }
+                
+                expectationSaveLocal?.fulfill()
+            case 1:
+                if let person = person {
+                    XCTAssertNotNil(person.personId)
+                    if let personId = person.personId {
+                        XCTAssertFalse(personId.hasPrefix(ObjectIdTmpPrefix))
+                    }
+                }
+                
+                expectationSaveNetwork?.fulfill()
+            default:
+                break
+            }
+            
+            runCount += 1
+        }
+        
+        waitForExpectations(timeout: defaultTimeout) { error in
+            expectationSaveLocal = nil
+            expectationSaveNetwork = nil
+        }
+    }
+    
+}
