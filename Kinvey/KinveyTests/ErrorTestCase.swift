d1dab15886effc8e253e378ac972941e48bf3042
Victor Barros <victor@kinvey.com>
2016-12-01 16:27:38 -0800

    Adding description and debug description properties
    Former-commit-id: e463a5c7797a8ded74505bbe3e985ef3a13b9d6c

--- Kinvey/KinveyTests/ErrorTestCase.swift
+++ Kinvey/KinveyTests/ErrorTestCase.swift
@@ -18,31 +18,31 @@ class ErrorTestCase: KinveyTestCase {
     }
     
     func testObjectIDMissing() {
-        XCTAssertEqual(Error.ObjectIdMissing.localizedDescription, "Object ID is required and is missing")
+        XCTAssertEqual("\(Error.ObjectIdMissing)", "Object ID is required and is missing")
     }
     
     func testInvalidResponse() {
-        XCTAssertEqual(Error.InvalidResponse(httpResponse: nil, data: nil).localizedDescription, "Invalid response from the server")
+        XCTAssertEqual("\(Error.InvalidResponse(httpResponse: nil, data: nil))", "Invalid response from the server")
     }
     
     func testUnauthorized() {
-        XCTAssertEqual(Error.Unauthorized(httpResponse: nil, data: nil, error: "Error", description: "Description").localizedDescription, "Description")
+        XCTAssertEqual("\(Error.Unauthorized(httpResponse: nil, data: nil, error: "Error", description: "Description"))", "Description")
     }
     
     func testNoActiveUser() {
-        XCTAssertEqual(Error.NoActiveUser.localizedDescription, "An active user is required and none was found")
+        XCTAssertEqual("\(Error.NoActiveUser)", "An active user is required and none was found")
     }
     
     func testRequestCancelled() {
-        XCTAssertEqual(Error.RequestCancelled.localizedDescription, "Request was cancelled")
+        XCTAssertEqual("\(Error.RequestCancelled)", "Request was cancelled")
     }
     
     func testInvalidDataStoreType() {
-        XCTAssertEqual(Error.InvalidDataStoreType.localizedDescription, "DataStore type does not support this operation")
+        XCTAssertEqual("\(Error.InvalidDataStoreType)", "DataStore type does not support this operation")
     }
     
     func testUserWithoutEmailOrUsername() {
-        XCTAssertEqual(Error.UserWithoutEmailOrUsername.localizedDescription, "User has no email or username")
+        XCTAssertEqual("\(Error.UserWithoutEmailOrUsername)", "User has no email or username")
     }
     
     func testInvalidResponseHttpResponseData() {
