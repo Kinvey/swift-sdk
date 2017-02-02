22e7dea4e27522277de4495b67637856a9d7d949
Victor Barros <victor@kinvey.com>
2017-02-02 13:13:25 -0800

    MLIBZ-1167: Geolocation Queries
    Former-commit-id: 29aa48b5120a863d08806ebf938b7b6465c46a51

---
+++ Kinvey/Kinvey/Geolocation.swift
@@ -0,0 +1,106 @@
+//
+//  Geolocation.swift
+//  Kinvey
+//
+//  Created by Victor Hugo on 2017-02-01.
+//  Copyright © 2017 Kinvey. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+import ObjectMapper
+import CoreLocation
+import MapKit
+
+open class GeoPoint: Object, Mappable {
+    
+    open dynamic var latitude: CLLocationDegrees = 0.0
+    open dynamic var longitude: CLLocationDegrees = 0.0
+    
+    public convenience required init?(map: Map) {
+        guard let _: Double = map["latitude"].value(), let _: Double = map["longitude"].value() else {
+            return nil
+        }
+        self.init()
+    }
+    
+    public convenience init(latitude: CLLocationDegrees, longitude: CLLocationDegrees) {
+        self.init()
+        self.latitude = latitude
+        self.longitude = longitude
+    }
+    
+    public convenience init(coordinate: CLLocationCoordinate2D) {
+        self.init(latitude: coordinate.latitude, longitude: coordinate.longitude)
+    }
+    
+    public convenience init(location: CLLocation) {
+        self.init(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+    }
+    
+    public convenience init(_ array: [CLLocationDegrees]) {
+        self.init(latitude: array[1], longitude: array[0])
+    }
+    
+    public func mapping(map: Map) {
+        latitude <- map["latitude"]
+        longitude <- map["longitude"]
+    }
+    
+}
+
+class GeoPointTransform: TransformOf<GeoPoint, [CLLocationDegrees]> {
+    
+    init() {
+        super.init(fromJSON: { (array) -> GeoPoint? in
+            if let array = array, array.count == 2 {
+                return GeoPoint(array)
+            }
+            return nil
+        }, toJSON: { (geopoint) -> [CLLocationDegrees]? in
+            if let geopoint = geopoint {
+                return [geopoint.longitude, geopoint.latitude]
+            }
+            return nil
+        })
+    }
+    
+}
+
+/// Override operator used during the `propertyMapping(_:)` method.
+public func <- (left: inout GeoPoint, right: (String, Map)) {
+    kinveyMappingType(left: right.0, right: right.1.currentKey!)
+    left <- (right.1, GeoPointTransform())
+}
+
+/// Override operator used during the `propertyMapping(_:)` method.
+public func <- (left: inout GeoPoint?, right: (String, Map)) {
+    kinveyMappingType(left: right.0, right: right.1.currentKey!)
+    left <- (right.1, GeoPointTransform())
+}
+
+/// Override operator used during the `propertyMapping(_:)` method.
+public func <- (left: inout GeoPoint!, right: (String, Map)) {
+    kinveyMappingType(left: right.0, right: right.1.currentKey!)
+    left <- (right.1, GeoPointTransform())
+}
+
+func ==(lhs: CLLocationCoordinate2D, rhs: CLLocationCoordinate2D) -> Bool {
+    return lhs.latitude == rhs.latitude && lhs.longitude == rhs.longitude
+}
+
+extension CLLocation {
+    
+    public convenience init(geoPoint: GeoPoint) {
+        self.init(latitude: geoPoint.latitude, longitude: geoPoint.longitude)
+    }
+    
+}
+
+extension CLLocationCoordinate2D {
+    
+    public init(geoPoint: GeoPoint) {
+        self.init(latitude: geoPoint.latitude, longitude: geoPoint.longitude)
+    }
+    
+}
