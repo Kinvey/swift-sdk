a6b8236e5cc0bfcce35ef0a60e812b08b819fdb4
Victor Barros <victor@kinvey.com>
2017-08-18 15:20:11 -0700

    reverting code since it introduced a bug

--- Kinvey/Kinvey/User.swift
+++ Kinvey/Kinvey/User.swift
@@ -1255,24 +1255,21 @@ open class User: NSObject, Credential, Mappable {
     static let MICSafariViewControllerSuccessNotificationName = NSNotification.Name("Kinvey.User.MICSafariViewController.Success")
     static let MICSafariViewControllerFailureNotificationName = NSNotification.Name("Kinvey.User.MICSafariViewController.Failure")
     
-    private static func removeObservers() {
-        if let token = MICSafariViewControllerSuccessNotificationObserver {
-            NotificationCenter.default.removeObserver(token, name: MICSafariViewControllerSuccessNotificationName, object: nil)
-        }
-        if let token = MICSafariViewControllerFailureNotificationObserver {
-            NotificationCenter.default.removeObserver(token, name: MICSafariViewControllerFailureNotificationName, object: nil)
-        }
-    }
-    
     private static var MICSafariViewControllerSuccessNotificationObserver: Any? = nil {
         willSet {
-            removeObservers()
+            if let token = MICSafariViewControllerSuccessNotificationObserver {
+                NotificationCenter.default.removeObserver(token, name: MICSafariViewControllerSuccessNotificationName, object: nil)
+                NotificationCenter.default.removeObserver(token, name: MICSafariViewControllerFailureNotificationName, object: nil)
+            }
         }
     }
     
     private static var MICSafariViewControllerFailureNotificationObserver: Any? = nil {
         willSet {
-            removeObservers()
+            if let token = MICSafariViewControllerFailureNotificationObserver {
+                NotificationCenter.default.removeObserver(token, name: MICSafariViewControllerSuccessNotificationName, object: nil)
+                NotificationCenter.default.removeObserver(token, name: MICSafariViewControllerFailureNotificationName, object: nil)
+            }
         }
     }
     
