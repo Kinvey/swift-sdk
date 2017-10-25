ab11637ad8185b5ac37d8073a436335671dd2cc9
Victor Hugo Barros <hey.zooi@gmail.com>
2017-10-25 10:55:04 -0700

    adding `all` validation strategy

--- Kinvey/Kinvey/ValidationStrategy.swift
+++ Kinvey/Kinvey/ValidationStrategy.swift
@@ -11,6 +11,9 @@ import Foundation
 /// Defines a strategy to validate results upfront
 public enum ValidationStrategy {
     
+    /// Validates all items in a backend response. Validating all entities upfront results in a performance penalty.
+    case all
+    
     /// Percentage between 0.0 and 1.0. This number specifies the fraction of entities in a backend response that are validated. Validating a higher percentage of entities upfront results in a performance penalty.
     case randomSample(percentage: Double)
     
@@ -19,22 +22,37 @@ public enum ValidationStrategy {
     
     func validate(jsonArray: Array<Dictionary<String, Any>>) -> Swift.Error? {
         switch self {
+        case .all:
+            for item in jsonArray {
+                if let error = validate(item: item) {
+                    return error
+                }
+            }
+            return nil
         case .randomSample(let percentage):
             let max = UInt32(jsonArray.count)
             let numberOfItems = min(Int(ceil(Double(jsonArray.count) * percentage)), jsonArray.count)
             for _ in 0 ..< numberOfItems {
                 let item = jsonArray[Int(arc4random_uniform(max))]
-                guard
-                    let id = item[Entity.Key.entityId] as? String,
-                    !id.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
-                else {
-                    return Error.objectIdMissing
+                if let error = validate(item: item) {
+                    return error
                 }
             }
             return nil
         case .custom(let validationBlock):
             return validationBlock(jsonArray)
         }
+    }
+    
+    @inline(__always)
+    private func validate(item: Dictionary<String, Any>) -> Swift.Error? {
+        guard
+            let id = item[Entity.Key.entityId] as? String,
+            !id.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
+        else {
+            return Error.objectIdMissing
+        }
+        return nil
     }
     
 }
